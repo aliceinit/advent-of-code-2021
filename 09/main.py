ceaa4328c32eb2d4b3ef62f03724de3ac171516a
Karen Hanson <karen.hanson@tutanota.com>
2021-12-12 10:14:08 -0400

    Add solutions for day 9 part 2

--- 09/main.py
+++ 09/main.py
@@ -31,6 +31,64 @@ def find_low_points(floor_map):
     return low_points
 
 
+def find_basins(floor_map):
+    """
+    Groups all points on a floor map into basins and calculates the size of the basin
+    Basins are groups of numbers bordered by nines (which are not part of any basin)
+    :param floor_map: list of same-length-lists defining coordinates
+    :return: list of int, representing basin sizes
+    """
+    basins = {}
+    basin_map = [[None] * len(floor_map[0])] * len(floor_map)
+    last_basin_id = 0
+
+    def mark_basin(i, j, basin_id, value):
+        if not basins.get(basin_id):
+            basins[basin_id] = []
+        basins[basin_id].append(value)
+        basin_map[i][j] = basin_id
+
+    for i, row in enumerate(floor_map):
+        for j, col in enumerate(row):
+            value = floor_map[i][j]
+
+            if value == 9:  # we are not part of a basin
+                basin_map[i][j] = None
+            else:
+                first_row = i == 0
+                first_col = j == 0
+
+                # Only look up and left
+                previous_neighbours = list(filter(lambda val: val is not None,
+                                                  [  # Get number above
+                                                      basin_map[i - 1][j] if not first_row else None,
+                                                      # Get number to the left
+                                                      basin_map[i][j - 1] if not first_col else None
+                                                  ]))
+                if len(previous_neighbours) == 0:
+                    # Neither neighbour is part of a basin -- start tracking a new one
+                    last_basin_id += 1
+                    mark_basin(i, j, last_basin_id, value)
+
+                elif len(previous_neighbours) == 1 or \
+                        previous_neighbours[0] == previous_neighbours[1]:
+                    # One of both of our neighbors are in a basin: add ourself to the same basin
+                    mark_basin(i, j, previous_neighbours[0], value)
+                elif previous_neighbours[0] != previous_neighbours[1]:
+                    # Our neighbours which were thought to be in different basins are actually
+                    # Part of the same basin: merge them and add ourselves
+                    mark_basin(i, j, previous_neighbours[0], value)
+                    basins[previous_neighbours[0]] += basins[previous_neighbours[1]]
+                    basins.pop(previous_neighbours[1])
+                    for x in range(0, i):
+                        for y in range(0, j):
+                            if basin_map[x][y] == previous_neighbours[1]:
+                                basin_map[x][y] = previous_neighbours[0]
+
+    return sorted([len(basin_vals)
+                   for basin_vals in basins.values()], reverse=True)
+
+
 def sum_risk_levels(values):
     """
     Returns the sum of the risk levels for provided points
@@ -52,3 +110,5 @@ if __name__ == '__main__':
         floor_map = read_floor_map(f)
         low_points = find_low_points(floor_map)
         print(f"Total Risk Score = {sum_risk_levels(low_points)}")
+        basins = find_basins(floor_map)
+        print(f"Three biggest basins: {basins[0:3]} (multiplied = {basins[0] * basins[1] * basins[2]})")
--- 09/test.py
+++ 09/test.py
@@ -4,7 +4,7 @@ from .main import *
 
 
 class TestSubCommands(TestCase):
-    sample_input = """2199943210
+    sample_input = """  2199943210
                         3987894921
                         9856789892
                         8767896789
@@ -23,3 +23,29 @@ class TestSubCommands(TestCase):
 
     def test_risk_calculation(self):
         self.assertEqual(15, sum_risk_levels([0, 1, 5, 5]))
+
+    def test_find_basins_sample(self):
+        floor_map = self.parse_input(self.sample_input)
+        basins = find_basins(floor_map)
+        self.assertEqual([14, 9, 9, 3],
+                         basins)
+
+    def test_find_basins_tricky(self):
+        input = """ 00900
+                    00900
+                    90009
+                    00900
+                    00900"""
+        floor_map = self.parse_input(input)
+        basins = find_basins(floor_map)
+        self.assertEqual([19], basins)
+
+    def test_find_basins_multiple(self):
+        input = """ 00900
+                    00900
+                    99999
+                    00900
+                    00900"""
+        floor_map = self.parse_input(input)
+        basins = find_basins(floor_map)
+        self.assertEqual([4, 4, 4, 4], basins)
