ba8f4b02e07ced771c697c623c833f24b7893c20
Karen Hanson <karen.hanson@tutanota.com>
2021-12-14 22:01:58 -0400

    add solution for day 10 part 2

--- 10/main.py
+++ 10/main.py
@@ -1,20 +1,21 @@
 from queue import LifoQueue
 
-CHAR_PAIR_LOOKUP = {
+CLOSE_TO_OPEN_CHAR = {
     "}": "{",
     ")": "(",
     ">": "<",
     "]": "["
 }
+OPEN_TO_CLOSE_CHAR = {v: k for k, v in CLOSE_TO_OPEN_CHAR.items()}
 
 
 def find_corrupted_character(syntax_line):
     character_queue = LifoQueue()
     for char in syntax_line:
-        if char in CHAR_PAIR_LOOKUP.keys():
+        if char in CLOSE_TO_OPEN_CHAR.keys():
             # Character is a closing character: check if it matches previous char in queue
             last_char = character_queue.get()
-            if not last_char == CHAR_PAIR_LOOKUP[char]:
+            if not last_char == CLOSE_TO_OPEN_CHAR[char]:
                 return char
         else:
             # Character is an opening character: add it to the queue
@@ -39,6 +40,60 @@ def find_syntax_error_score(lines):
     ])
 
 
+def autocomplete_line(syntax_line):
+    character_queue = LifoQueue()
+    for char in syntax_line:
+        if char in CLOSE_TO_OPEN_CHAR.keys():
+            # Character is a closing character; remove char from queue
+            pair = character_queue.get()
+            if pair != CLOSE_TO_OPEN_CHAR[char]:
+                raise RuntimeError("Cannot autocomplete corrupted line")
+        else:
+            # Character is an opening character: add it to the queue
+            character_queue.put(char)
+
+    # After reaching the end of the line, auto-complete remaining characters:
+    remaining_chars = []
+
+    while not character_queue.empty():
+        char = character_queue.get()
+        remaining_chars.append(OPEN_TO_CLOSE_CHAR[char])
+    return remaining_chars
+
+
+AUTOCOMPLETE_SCORES = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4
+}
+
+
+def get_auto_complete_line_score(line):
+    remaining_chars = autocomplete_line(line)
+    score = 0
+    for c in remaining_chars:
+        score = score * 5 + AUTOCOMPLETE_SCORES[c]
+    return score
+
+
+def return_middle_val(val_list):
+    return val_list[int(len(val_list) / 2)]
+
+
+def get_auto_complete_score(lines):
+    scores = sorted([get_auto_complete_line_score(l)
+                     for l in get_incomplete_lines(lines)])
+    # Return middle score
+    return return_middle_val(scores)
+
+
+def get_incomplete_lines(lines):
+    """Find and discard all lines with corrupted characters. Return remaining lines"""
+    return [l for l in lines
+            if find_corrupted_character(l) is None]
+
+
 def read_nav_subsystem_lines(file):
     return [l.strip() for l in file.readlines()]
 
@@ -46,5 +101,9 @@ def read_nav_subsystem_lines(file):
 if __name__ == '__main__':
     with open("puzzle_1_input.text", "r") as f:
         lines = read_nav_subsystem_lines(f)
-        score = find_syntax_error_score(lines)
-        print(f"Syntax error score: {score}")
+        error_score = find_syntax_error_score(lines)
+        print(f"Syntax error score: {error_score}")
+
+        autocomplete_score = get_auto_complete_score(lines)
+        print(f"Auto-complete score: {autocomplete_score}")
+
--- 10/test.py
+++ 10/test.py
@@ -25,3 +25,30 @@ class TestSubCommands(TestCase):
         score = find_syntax_error_score(lines)
         self.assertEqual(26397, score)
 
+    def test_sample_input_find_incomplete(self):
+        lines = self.parse_input(self.sample_input)
+        incomplete_lines = get_incomplete_lines(lines)
+        self.assertEqual(["[({(<(())[]>[[{[]{<()<>>",
+                          "[(()[<>])]({[<{<<[]>>(",
+                          "(((({<>}<{<{<>}{[]{[]{}",
+                          "{<[[]]>}<{[{[{[]{()[[[]",
+                          "<{([{{}}[<[[[<>{}]]]>[]]"],
+                         ["".join(line) for line in incomplete_lines])
+
+    def test_sample_autocomplete(self):
+        self.assertEqual("}}]])})]",
+                         "".join(autocomplete_line("[({(<(())[]>[[{[]{<()<>>")))
+
+    def test_autocomplete_line_score(self):
+        self.assertEqual(288957,
+                         get_auto_complete_line_score("[({(<(())[]>[[{[]{<()<>>"))
+
+    def test_sample_input_find_autocomplete_score(self):
+        lines = self.parse_input(self.sample_input)
+        self.assertEqual(288957,
+                         get_auto_complete_score(lines))
+
+    def test_return_middle_val(self):
+        self.assertEqual(3, return_middle_val([1, 2, 3, 4, 5]))
+        self.assertEqual(4, return_middle_val([1, 2, 3, 4, 5, 6, 7]))
+        self.assertEqual(5, return_middle_val([1, 2, 3, 4, 5, 6, 7, 8, 9]))
