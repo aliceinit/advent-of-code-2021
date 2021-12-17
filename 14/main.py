f9ffcaabe65df4ac5cf6bf36303a0c885c128cc0
Karen Hanson <karen.hanson@tutanota.com>
2021-12-17 14:59:56 -0400

    Add solution for day 14 part 2

--- 14/main.py
+++ 14/main.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from collections import deque
 
 
 class PolymerElement:
@@ -15,67 +14,132 @@ class PolymerElement:
             next = next.next
         return polymer
 
+    def get_letter_counts(self):
+        counts = {}
+        node = self
+        while node is not None:
+            if not counts.get(node.letter):
+                counts[node.letter] = 1
+            else:
+                counts[node.letter] += 1
+            node = node.next
+        return counts
 
-def process_insertion_step(polymer_chain, rules):
-    current_element = polymer_chain
-    while current_element.next is not None:
-        insert = PolymerElement(rules.get(current_element.letter + current_element.next.letter))
-
-        # Insert new element
-        insert.next = current_element.next
-        current_element.next = insert
-
-        # Advance to next element
-        current_element = insert.next
-
-    return polymer_chain
-
-
-def process_insertion_single_traversal(polymer, rules, step_count):
-    current_element = polymer
-    processed_chars = 0
-
-    def insert_between(this, next):
-        insert = PolymerElement(rules[this.letter + next.letter])
-        insert.next = next
-        this.next = insert
-
-    while current_element.next is not None:
-        start_time = datetime.now()
-        left_anchor = current_element
-        right_anchor = current_element.next
-        for depth in range(step_count):
-
-            current = left_anchor
-            next = left_anchor.next
-            while next != right_anchor:
-                insert_between(current, next)
-                current = next
-                next = next.next
-
-            # One final insert
-            insert_between(current, right_anchor)
-            print(f"Processed char {processed_chars} depth = {depth} in {(datetime.now() - start_time).total_seconds()}")
-        current_element = right_anchor
-        print(f"Processed char {processed_chars} in {(datetime.now() - start_time).total_seconds()}")
-        processed_chars += 1
-
-    return polymer
+    def drop_final_character(self):
+        previous = self
+        next = self.next
+        while next.next is not None:
+            previous = next
+            next = next.next
+        previous.next = None
 
 
-def process_polymer_insertions(polymer, rules, step_count):
-    for i in range(step_count):
-        polymer = process_insertion_step(polymer, rules)
-    return polymer
+class Polymer(PolymerElement):
+    def __init__(self, string):
+        super().__init__(string[0])
+        self.next = PolymerElement(string[1])
+        last = self.next
+        if len(string) > 2:
+            for s in string[2:]:
+                element = PolymerElement(s)
+                last.next = element
+                last = element
 
 
-def find_most_and_least_common_elements(polymer):
-    counts = {}
-    for char in polymer:
-        if not counts.get(char):
-            counts[char] = 1
-        else:
-            counts[char] += 1
+def insert_between(left: PolymerElement, right: PolymerElement, insert):
+    """Insert one or more elements between two others"""
+    previous = left
+    if isinstance(insert, str):
+        for letter in insert:
+            next = PolymerElement(letter)
+            previous.next = next
+            previous = next
+    elif isinstance(insert, PolymerElement):
+        left.next = insert
+        previous = insert
+        next = insert.next
+        while next is not None:
+            previous = next
+            next = previous.next
+
+    previous.next = right
+
+
+def expand_polymer(args):
+    first_element, rules = args
+
+    left = first_element
+    right = first_element.next
+    while right is not None:
+        insert_between(left, right,
+                       rules[f"{left.letter}{right.letter}"])
+        left = right
+        right = right.next
+    return first_element
+
+
+def expand_rules(rules, step_count):
+    i = 1
+    start_time = datetime.now()
+    new_rules = {pair: Polymer(f"{pair[0]}{rules[pair]}{pair[1]}") for pair in rules.keys()}
+    while i < step_count:
+        for pair in rules.keys():
+            expand_polymer((new_rules[pair], rules))
+        i += 1
+    t = (datetime.now() - start_time).total_seconds()
+    # Drop first letter from expansions
+    new_rules = {pair: new_rules[pair].next for pair in new_rules.keys()}
+    # Drop final letter from expansions
+    for expansion in new_rules.values():
+        expansion.drop_final_character()
+
+    print(f"Finished creating expansion of rules to depth {step_count} in {t} seconds")
+    return new_rules
+
+
+def find_most_and_least_common_elements(polymer, insertion_rules, step_count):
+    print("Expanding rules...")
+    expanded_rules = expand_rules(insertion_rules, int(step_count / 2))
+    # Pre-calculate counts for expansions
+    print("Calculating expansion counts...")
+    expansion_counts = {pair: expansion.get_letter_counts()
+                        for pair, expansion in expanded_rules.items()}
+
+    # convert expansions to string:
+    expanded_rules = {pair: str(expansion)
+                      for pair, expansion in expanded_rules.items()}
+
+    if step_count % 2 == 1:
+        # Account for odd number by expanding initial polymer once
+        print("Performing additional expansion for uneven step count")
+        expand_polymer((polymer, insertion_rules))
+
+    # Expand polymer to its half-way point using expanded rules:
+    print("Performing final expansion...")
+    expand_polymer((polymer, expanded_rules))
+
+    # Set up for tracking counts
+    letters = set()
+    for pair in expanded_rules.keys():
+        letters.add(pair[0])
+        letters.add(pair[1])
+    counts = {l: 0 for l in letters}
+
+    print("Starting Counts...")
+    # For each pair, count the left char + substitution
+    left = polymer
+    right = polymer.next
+
+    while right is not None:
+        counts[left.letter] += 1
+        inner_count = expansion_counts[f"{left.letter}{right.letter}"]
+        for letter, count in inner_count.items():
+            counts[letter] += count
+        left = right
+        right = right.next
+
+    # Count the very last character
+    counts[left.letter] += 1
 
     sorted_counts = sorted([(k, v) for k, v in counts.items()],
                            key=lambda x: x[1])
@@ -92,22 +156,20 @@ def parse_polymer_and_rules(file):
         pair, result = line.split(" -> ")
         rules[pair] = result
 
-    first_element = PolymerElement(lines[0][0])
-    last_element = first_element
-    for char in lines[0][1:]:
-        next_element = PolymerElement(char)
-        last_element.next = next_element
-        last_element = next_element
-    return first_element, rules
+    return Polymer(lines[0]), rules
 
 
 if __name__ == '__main__':
     with open("puzzle_1_input.text", "r") as f:
         polymer, rules = parse_polymer_and_rules(f)
-        process_polymer_insertions(polymer, rules, 10)
-        _, most_count, _, least_count = find_most_and_least_common_elements(str(polymer))
+        polymer_part_2 = Polymer(str(polymer))
+        print(polymer_part_2)
+
+        _, most_count, _, least_count = find_most_and_least_common_elements(
+            polymer, rules, 10)
+
         print(f"most minus least after 10 rounds: {most_count - least_count}")
 
-        process_insertion_single_traversal(polymer, rules, 40)
-        _, most_count_40, _, least_count_40 = find_most_and_least_common_elements(str(polymer))
+        _, most_count_40, _, least_count_40 = find_most_and_least_common_elements(
+            polymer_part_2, rules, 40)
         print(f"most minus least after 40 rounds: {most_count_40 - least_count_40}")
--- 14/test.py
+++ 14/test.py
@@ -30,27 +30,66 @@ class TestSubCommands(TestCase):
 
     def test_sample_data(self):
         polymer, insertion_rules = self.parse_input(self.sample_input)
-        process_insertion_step(polymer, insertion_rules)
+        expand_polymer((polymer, insertion_rules))
         self.assertEqual("NCNBCHB", str(polymer))
 
-        process_insertion_step(polymer, insertion_rules)
+        expand_polymer((polymer, insertion_rules))
         self.assertEqual("NBCCNBBBCBHCB", str(polymer))
 
-    def test_multi_step(self):
-        polymer, insertion_rules = self.parse_input(self.sample_input)
-        process_polymer_insertions(polymer, insertion_rules, 2)
-        self.assertEqual("NBCCNBBBCBHCB", str(polymer))
+    def test_build_expansion_table(self):
+        _, insertion_rules = self.parse_input(self.sample_input)
+        new_rules = expand_rules(insertion_rules, 5)
 
-    def test_single_traversal(self):
-        polymer, insertion_rules = self.parse_input(self.sample_input)
-        process_insertion_single_traversal(polymer, insertion_rules, 2)
-        self.assertEqual("NBCCNBBBCBHCB", str(polymer))
+        self.assertIsInstance(new_rules["NN"], PolymerElement)
+        for pair, expansion in new_rules.items():
+            print(f"{pair}: {expansion}")
+            self.assertEqual(31, len(str(expansion)), f"Wrong result length for pair '{pair}'")
+
+    def test_count_chars(self):
+        polymer = Polymer("ABBCCCD")
+        counts = polymer.get_letter_counts()
+        self.assertEqual({"A": 1, "D": 1, "B": 2, "C": 3}, counts)
+
+    def test_drop_final_char(self):
+        polymer = Polymer("ABCD")
+        self.assertEqual("ABCD", str(polymer))
+        polymer.drop_final_character()
+        self.assertEqual("ABC", str(polymer))
+
+    def test_insert_string_between(self):
+        pair = Polymer("AB")
+        insert_between(pair, pair.next, "CDE")
+        self.assertEqual("ACDEB", str(pair))
+
+    def test_insert_polymer_between(self):
+        pair = Polymer("AB")
+        inner = Polymer("CDE")
+        insert_between(pair, pair.next, inner)
+        self.assertEqual("ACDEB", str(pair))
 
     def test_most_and_least_common_elements(self):
         polymer, insertion_rules = self.parse_input(self.sample_input)
-        process_polymer_insertions(polymer, insertion_rules, 10)
         most_common_element, most_common_count, least_common_element, least_common_count = \
-            find_most_and_least_common_elements(str(polymer))
-        assert most_common_element == "B"
-        assert least_common_element == "H"
-        assert most_common_count - least_common_count == 1588
+            find_most_and_least_common_elements(polymer, insertion_rules, 10)
+        self.assertEqual("B", most_common_element)
+        self.assertEqual("H", least_common_element)
+        self.assertEqual(1749, most_common_count)
+        self.assertEqual(161, least_common_count)
+        self.assertEqual(1588, most_common_count - least_common_count)
+
+    # def test_most_and_least_common_elements_at_40(self):
+    #     polymer, insertion_rules = self.parse_input(self.sample_input)
+    #     most_common_element, most_common_count, least_common_element, least_common_count = \
+    #         find_most_and_least_common_elements(polymer, insertion_rules, 40)
+    #     assert most_common_element == "B"
+    #     assert least_common_element == "H"
+    #     assert most_common_count - least_common_count == 2188189693529
+
+    # def test_expansion_table_to_18(self):
+    #     _, insertion_rules = self.parse_input(self.sample_input)
+    #     build_expansion_table(insertion_rules, 19)
+    #
+    # def test_expand_to_18(self):
+    #     """ Test that we can expand rules to depth 20 with decent speed"""
+    #     polymer, insertion_rules = self.parse_input(self.sample_input)
+    #     expand_polymer(polymer, insertion_rules, 18)
